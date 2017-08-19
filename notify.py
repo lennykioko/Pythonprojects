701a080c15dec8e80a8b5c844df075e3b21014b2
unknown <lennykmutua@gmail.com>
2017-08-19 16:51:43 +0300

    First commit

---
+++ notify.py
@@ -0,0 +1,20 @@
+#! python3
+# notify.py - Defines the  notify() function that texts a message
+# passed to it as a string.
+from twilio.rest import Client
+
+
+account_sid = 'xxxxxxxxxxxxxxxxxx'
+auth_token = 'xxxxxxxxxxxxx'
+my_cell = 'xxxxxxxxxxx'
+my_twilio = 'xxxxxxxxxxx'
+   
+
+def notify(message):
+ 	client = Client( account_sid, auth_token)
+ 	client.messages.create(to=my_cell, from_=my_twilio, body=message)
+
+
+#just add this to the program you need to be notified once it finishes running
+#import Notification
+#Notification.notify('The boring task is finished.')   
